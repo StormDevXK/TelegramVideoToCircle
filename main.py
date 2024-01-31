bb2c6950a12f0e277609b10d18d7ff76dbd582c4
SrormDevXK <mhstudio1912@gmail.com>
2024-02-01 01:35:24 +0300

    add try except and message Идет обработка видео

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types, exceptions
 import config
 from video_edit import cut_video
 
@@ -13,17 +13,20 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(content_types=['video'])
 async def save_video(message: types.Message):
-    file_id = message.video.file_id # Get file id
-    file = await bot.get_file(file_id) # Get file path
-    await bot.download_file(file.file_path, "video.mp4")
-    cut_video('video.mp4', 'vid.mp4')
-    vid = open('vid.mp4', 'rb')
-    await bot.send_video_note(message.chat.id, vid)
+    file_id = message.video.file_id
+    try:
+        file = await bot.get_file(file_id)
+        await message.reply('Идет обработка видео...')
+        await bot.download_file(file.file_path, "video.mp4")
+        cut_video('video.mp4', 'vid.mp4')
+        vid = open('vid.mp4', 'rb')
+        await bot.send_video_note(message.chat.id, vid)
+    except exceptions.FileIsTooBig:
+        await bot.send_message(message.chat.id, 'Размер файла слишком большой')
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    # vid = open('video.mp4', 'rb')
     vid = open('vid.mp4', 'rb')
     await bot.send_video_note(message.chat.id, vid)
     await message.answer(message.text)
