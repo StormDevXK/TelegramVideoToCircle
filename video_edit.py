100504c2e886030ffef03116edd75959a0fd25a8
SrormDevXK <mhstudio1912@gmail.com>
2024-02-01 23:56:15 +0300

    Now the circle is not cut off, but changes size

--- video_edit.py
+++ video_edit.py
@@ -1,11 +1,17 @@
 import moviepy
 import moviepy.editor as mpe
-import moviepy.video.fx.all as mpec
+import moviepy.video.fx.all as vfx
 
 
 def cut_video(en: str, ex: str):
     clip = mpe.VideoFileClip(en)
     cs = clip.size
     print(cs, (cs[0] - 576) // 2, (cs[1] - 576) // 2)
-    res = mpec.crop(clip, x1=(cs[0] - 576) // 2, width=576, y1=(cs[1] - 576) // 2, height=576)
+    # res = vfx.crop(clip, x1=(cs[0] - 576) // 2, width=576, y1=(cs[1] - 576) // 2, height=576)
+    if cs[0] < cs[1]:
+        res = vfx.resize(clip, width=576)
+        res = vfx.crop(res, y1=(res.size[1] - 576) // 2,  height=576)
+    else:
+        res = vfx.resize(clip, height=576)
+        res = vfx.crop(res, x1=(res.size[0] - 576) // 2,  width=576)
     res.write_videofile(ex)
