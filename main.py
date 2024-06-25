1c7226215e77ae5909a19d931f47c280eb325509
SrormDevXK <mhstudio1912@gmail.com>
2024-06-25 04:54:56 +0300

    индивидуальное название каждого видео, эффекты на кружок

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from aiogram import Bot, Dispatcher, executor, types, exceptions
 import config
 from video_edit import cut_video
 from loger import log_bot
+import time
+import os
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
@@ -10,7 +12,22 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     log_bot(message.chat.id, message.from_user.username, 'send_welcome', message.text)
-    await message.reply("Привет!\nЯ бот, с помощью которого ты сможешь преобразовать видео в кружочек!")
+    await bot.send_message(message.chat.id, "Привет!\nЯ бот, с помощью которого ты сможешь преобразовать видео в "
+                                            "кружочек!\nПросто отправь мне видео, а я пришлю тебе результат.")
+    await bot.send_message(message.chat.id, 'Для получения дополнительной информации воспользуйтесь командой /help')
+
+
+@dp.message_handler(commands=['help'])
+async def helps(message: types.Message):
+    log_bot(message.chat.id, message.from_user.username, 'help', message.text)
+    await bot.send_message(message.chat.id, "Рекомендации для входного видео:\n• Длительность видео должна быть менее "
+                                            "1 минуты.\n• Размер файла не должен превышать 20 мегабайт (МБ).\n• При "
+                                            "установке ширины и высоты видео увеличение размеров бесполезно, "
+                                            "поскольку максимальный размер видеосообщения составляет 576 пикселей. "
+                                            "Рекомендуется обрезать видео вручную, сделав его квадратным, "
+                                            "перед загрузкой. Также возможно внесение изменений непосредственно в "
+                                            "редакторе Telegram.\n• Рекомендуемая частота кадров - 30 кадров в "
+                                            "секунду.")
 
 
 @dp.message_handler(content_types=['video'])
@@ -20,16 +37,22 @@ async def video_circle(message: types.Message):
     try:
         file = await bot.get_file(file_id)
         await message.reply('Идет обработка видео...')
-        await bot.download_file(file.file_path, "video.mp4")
-        cut_video('video.mp4', 'vid.mp4')
-        vid = open('vid.mp4', 'rb')
+        video_title = f'video_storage/{message.from_user.id}_{int(time.time()*100)}'
+        vide = await bot.download_file(file.file_path, f'{video_title}.mp4')
+        await cut_video(f'{video_title}.mp4', f'{video_title}ex.mp4')
+        vid = open(f'{video_title}ex.mp4', 'rb')
         await bot.send_video_note(message.chat.id, vid)
+        vide.close()  # ERROR: файл не удаляется. неопределенный процесс не дает его удалить
+        vid.close()
+        os.remove(f'{video_title}ex.mp4')
+        os.remove(f'{video_title}.mp4')
     except exceptions.FileIsTooBig:
         await bot.send_message(message.chat.id, 'Размер файла слишком большой')
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
+    print(f'{message.from_user.id}_{int(time.time()*100)}')
     log_bot(message.chat.id, message.from_user.username, 'echo', message.text)
     # vid = open('vid.mp4', 'rb')
     # await bot.send_video_note(message.chat.id, vid)
--- video_edit.py
+++ video_edit.py
@@ -1,17 +1,42 @@
 import moviepy
 import moviepy.editor as mpe
 import moviepy.video.fx.all as vfx
+import ffmpeg
 
 
-def cut_video(en: str, ex: str):
-    clip = mpe.VideoFileClip(en)
+def add_image(clip, img_name: str):
+    img = (mpe.ImageClip(f'imgs/{img_name}.png')
+           .set_duration(clip.duration)
+           # .resize(height=50)  # если необходимо поменять размер...
+           # .margin(right=8, top=8, opacity=0)  # (опционально) logo-border padding
+           .set_pos(('center', 'center')))
+    final = mpe.CompositeVideoClip([clip, img])
+    return final
+
+
+async def cut_video(en: str, ex: str, img: str = None):
+    cclip = mpe.VideoFileClip(en)
+    clip = cclip.set_fps(30)
     cs = clip.size
     print(cs, (cs[0] - 576) // 2, (cs[1] - 576) // 2)
-    # res = vfx.crop(clip, x1=(cs[0] - 576) // 2, width=576, y1=(cs[1] - 576) // 2, height=576)
     if cs[0] < cs[1]:
         res = vfx.resize(clip, width=576)
-        res = vfx.crop(res, y1=(res.size[1] - 576) // 2,  height=576)
+        res = vfx.crop(res, y1=(res.size[1] - 576) // 2, height=576)
     else:
         res = vfx.resize(clip, height=576)
-        res = vfx.crop(res, x1=(res.size[0] - 576) // 2,  width=576)
+        res = vfx.crop(res, x1=(res.size[0] - 576) // 2, width=576)
+    if img is not None:
+        res = add_image(res, img)
     res.write_videofile(ex)
+
+
+# stream = ffmpeg.input('video.mp4')
+# stream = ffmpeg.filter(stream, 'fps', fps=10, round='up')
+# stream = ffmpeg.output(stream, 'out.mp4')
+# ffmpeg.run(stream)
+# abs = ffmpeg.probe('out.mp4')
+
+# stream = ffmpeg.input('input.mp4')
+# stream = ffmpeg.hflip(stream)
+# stream = ffmpeg.output(stream, 'output.mp4')
+# ffmpeg.run(stream)
